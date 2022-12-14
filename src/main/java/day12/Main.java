9c837cb2a3ac1a01bf0272323fa00d6e7a2bb78c
antraxo-git <mkuzma1998@gmail.com>
2022-12-14 01:31:17 +0100

    Day 12 Working

--- src/main/java/day12/Main.java
+++ src/main/java/day12/Main.java
@@ -13,32 +13,20 @@ public class Main {
         File file = new File("src/main/java/day12/input.txt");
 
         try {
-            Character[][] grid = Files.lines(file.toPath())
+            Character[][] grid1 = Files.lines(file.toPath())
                     .map(s -> Arrays.stream(s.split(""))
                             .map(s2 -> s2.charAt(0))
                             .toArray(Character[]::new))
                     .toArray(Character[][]::new);
 
-            for(Character[] add : grid) {
-                System.out.println(Arrays.toString(add));
-            }
+            Character[][] grid2 = Files.lines(file.toPath())
+                    .map(s -> Arrays.stream(s.split(""))
+                            .map(s2 -> s2.charAt(0))
+                            .toArray(Character[]::new))
+                    .toArray(Character[][]::new);
 
-            Deque<Position> posToCheck = new LinkedList<>();
-            List<Position> checkedPos = new ArrayList<>();
-            Position startingPoint = new Position(0,0, 0);
-            posToCheck.add(startingPoint);
-
-            while(posToCheck.size() != 0) {
-                Position pos = posToCheck.getFirst();
-                posToCheck.removeFirst();
-                if(checkIfEnd(pos, grid)) {
-                    System.out.println("Steps: " + pos.steps);
-                    break;
-                }
-                checkedPos.add(pos);
-                posToCheck.addAll(findNeighbors(pos, grid, checkedPos));
-                System.out.println(posToCheck.size());
-            }
+            part1(grid1);
+            part2(grid2);
 
 
         } catch (IOException e) {
@@ -46,36 +34,157 @@ public class Main {
         }
     }
 
-    static List<Position> findNeighbors(Position point, Character[][] grid, List<Position> checkedPos) {
-        char posValue = grid[point.x][point.y];
-        if(posValue == 'S') {
-            posValue = 'a';
+    static void part2(Character[][] grid) {
+        Position startingPoint = new Position(0,0, 0);
+
+        for(int y=0; y < grid.length; y++) {
+            for(int x=0; x < grid[0].length; x++) {
+                if(grid[y][x] == 'E') {
+                    grid[y][x] = 'z';
+                    startingPoint.x = x;
+                    startingPoint.y = y;
+                }
+            }
         }
+
+        Deque<Position> posToCheck = new LinkedList<>();
+        HashSet<Position> checkedPos = new HashSet<>();
+        posToCheck.add(startingPoint);
+
+        while(posToCheck.size() != 0) {
+            Position pos = posToCheck.removeFirst();
+            if(checkIfEnd2(pos, grid)) {
+                System.out.println("Steps: " + pos.steps);
+                break;
+            }
+            posToCheck.addAll(findNeighbors2(pos, grid, checkedPos));
+        }
+    }
+
+    static void part1(Character[][] grid) {
+        Position startingPoint = new Position(0,0, 0);
+        Position endingPoint = new Position(0,0, 0);
+
+        for(int y=0; y < grid.length; y++) {
+            for(int x=0; x < grid[0].length; x++) {
+                if(grid[y][x] == 'S') {
+                    grid[y][x] = 'a';
+                    startingPoint.x = x;
+                    startingPoint.y = y;
+                }
+                if(grid[y][x] == 'E') {
+                    grid[y][x] = 'z';
+                    endingPoint.x = x;
+                    endingPoint.y = y;
+                }
+            }
+        }
+
+        Deque<Position> posToCheck = new LinkedList<>();
+        HashSet<Position> checkedPos = new HashSet<>();
+        posToCheck.add(startingPoint);
+
+        while(posToCheck.size() != 0) {
+            Position pos = posToCheck.removeFirst();
+            if(checkIfEnd(pos, endingPoint)) {
+                System.out.println("Steps: " + pos.steps);
+                break;
+            }
+            posToCheck.addAll(findNeighbors(pos, grid, checkedPos));
+        }
+    }
+
+    static List<Position> findNeighbors2(Position point, Character[][] grid, HashSet<Position> checkedPos) {
+        char posValue = grid[point.y][point.x];
         List<Position> neighbors = new ArrayList<>();
-        if(point.getX() < grid.length - 1 ) {
-            if(!(posValue - grid[point.x + 1][point.y] < -1)) {
-                neighbors.add(new Position(point.x + 1, point.y, point.getSteps() + 1));
+        if(point.getY() < grid.length - 1 ) {
+            if(!(posValue - grid[point.y + 1][point.x] > 1)) {
+                Position posToAdd = new Position(point.x, point.y + 1, point.getSteps() + 1);
+                if(!checkedPos.contains(posToAdd)) {
+                    neighbors.add(posToAdd);
+                    checkedPos.add(posToAdd);
+                }
             }
         }
-        if(point.getY() < grid.length - 1) {
-            if(!(posValue - grid[point.x][point.y + 1] < -1)) {
-                neighbors.add(new Position(point.x, point.y + 1, point.getSteps() + 1));
-            }
-        }
-        if(point.getY() > 0) {
-            if (!(posValue - grid[point.x][point.y - 1] < -1)) {
-                neighbors.add(new Position(point.x, point.y - 1, point.getSteps() + 1));
+        if(point.getX() < grid[0].length - 1) {
+            if(!(posValue - grid[point.y][point.x + 1] > 1)) {
+                Position posToAdd = new Position(point.x + 1, point.y, point.getSteps() + 1);
+                if(!checkedPos.contains(posToAdd)) {
+                    neighbors.add(posToAdd);
+                    checkedPos.add(posToAdd);
+                }
             }
         }
         if(point.getX() > 0) {
-            if (!(posValue - grid[point.x - 1][point.y] < -1)) {
-                neighbors.add(new Position(point.x - 1, point.y, point.getSteps() + 1));
+            if (!(posValue - grid[point.y][point.x - 1] > 1)) {
+                Position posToAdd = new Position(point.x - 1, point.y , point.getSteps() + 1);
+                if(!checkedPos.contains(posToAdd)) {
+                    neighbors.add(posToAdd);
+                    checkedPos.add(posToAdd);
+                }
             }
         }
-        return neighbors.stream().filter(n -> !checkedPos.contains(n)).collect(Collectors.toList());
+        if(point.getY() > 0) {
+            if (!(posValue - grid[point.y - 1][point.x] > 1)) {
+                Position posToAdd = new Position(point.x, point.y - 1, point.getSteps() + 1);
+                if(!checkedPos.contains(posToAdd)) {
+                    neighbors.add(posToAdd);
+                    checkedPos.add(posToAdd);
+                }
+            }
+        }
+
+        return neighbors;
+    }
+
+    static boolean checkIfEnd2(Position point, Character[][] grid) {
+        return grid[point.y][point.x] == 'a';
+    }
+
+    static List<Position> findNeighbors(Position point, Character[][] grid, HashSet<Position> checkedPos) {
+        char posValue = grid[point.y][point.x];
+        List<Position> neighbors = new ArrayList<>();
+        if(point.getY() < grid.length - 1 ) {
+            if(!(posValue - grid[point.y + 1][point.x] < -1)) {
+                Position posToAdd = new Position(point.x, point.y + 1, point.getSteps() + 1);
+                if(!checkedPos.contains(posToAdd)) {
+                    neighbors.add(posToAdd);
+                    checkedPos.add(posToAdd);
+                }
+            }
+        }
+        if(point.getX() < grid[0].length - 1) {
+            if(!(posValue - grid[point.y][point.x + 1] < -1)) {
+                Position posToAdd = new Position(point.x + 1, point.y, point.getSteps() + 1);
+                if(!checkedPos.contains(posToAdd)) {
+                    neighbors.add(posToAdd);
+                    checkedPos.add(posToAdd);
+                }
+            }
+        }
+        if(point.getX() > 0) {
+            if (!(posValue - grid[point.y][point.x - 1] < -1)) {
+                Position posToAdd = new Position(point.x - 1, point.y , point.getSteps() + 1);
+                if(!checkedPos.contains(posToAdd)) {
+                    neighbors.add(posToAdd);
+                    checkedPos.add(posToAdd);
+                }
+            }
+        }
+        if(point.getY() > 0) {
+            if (!(posValue - grid[point.y - 1][point.x] < -1)) {
+                Position posToAdd = new Position(point.x, point.y - 1, point.getSteps() + 1);
+                if(!checkedPos.contains(posToAdd)) {
+                    neighbors.add(posToAdd);
+                    checkedPos.add(posToAdd);
+                }
+            }
+        }
+
+        return neighbors;
     }
     
-    static boolean checkIfEnd(Position point, Character[][] grid) {
-        return grid[point.x][point.y].equals('E');
+    static boolean checkIfEnd(Position point, Position endingPoint) {
+        return point.x == endingPoint.x && point.y == endingPoint.y;
     }
 }
--- src/main/java/day12/Position.java
+++ src/main/java/day12/Position.java
@@ -1,19 +1,44 @@
 package day12;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.awt.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Objects;
 
 @Getter
 @Setter
+@EqualsAndHashCode
 public class Position extends Point {
     int steps;
 
     public Position(int x, int y, int steps) {
         super(x, y);
         this.steps = steps;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(o instanceof Position) {
+            Position pos = (Position) o;
+            return x == pos.x && y == pos.y;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode());
+    }
+
+    @Override
+    public String toString() {
+        return "Position{" +
+                "steps=" + steps +
+                ", x=" + x +
+                ", y=" + y +
+                '}';
     }
 }
